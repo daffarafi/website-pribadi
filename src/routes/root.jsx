fdef623363a000001028dc114a620441613c90ad
Muhammad Daffa'I Rafi Prasetyo <daffarafip24@gmail.com>
2022-11-05 02:55:41 +0700

    Memperbaiki Padding dan Menu button

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -13,8 +13,8 @@ const Root = () => {
 				<div className='shape absolute w-[28rem] animate-[reversespin6_24s_linear_infinite] aspect-square bg-gradient-to-b from-[#2d49ff] to-[#0DCCFF] opacity-20' />
 			</div>
 			<nav className='fixed top-0 w-full '>
-				<div className='container grid grid-cols-3 items-center py-7'>
-					<Link className='relative text-xl font-semibold flex items-center select-none'>
+				<div className='container grid grid-cols-3 items-center pt-7'>
+					<Link className='relative w-fit text-xl font-semibold flex items-center select-none'>
 						<div className='hamburger flex flex-col w-9 h-5 justify-between justify-self-start'>
 							<div className='h-0.5 bg-slate-50 animate-[shrink_2s_linear_infinite]' />
 							<div className='h-0.5 bg-slate-50 animate-[shrink_2.5s_linear_infinite]' />
@@ -35,7 +35,7 @@ const Root = () => {
 			</nav>
 			<Outlet />
 			<div className='undernav fixed bottom-0 w-full'>
-				<div className='container flex justify-between py-7'>
+				<div className='container flex justify-between pb-7'>
 					<Link className='text-xl font-semibold w-fit h-fit flex items-center justify-self-end group'>
 						{' '}
 						<span className='block h-0 -mr-0.5 border-8 border-transparent border-l-[#2EE0D0] group-hover:animate-[pointbounce_0.5s_ease-out_infinite_alternate] group-hover:border-l-[#FF4444] transition-all duration-300 select-none' />
