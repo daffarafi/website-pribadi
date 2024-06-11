14bb167fce219deca657189a2c911e7ee1352643
daffarafip24 <daffarafip24@gmail.com>
2024-06-11 10:49:59 +0700

    add new portfolio

--- src/data/data.js
+++ src/data/data.js
@@ -1,48 +1,170 @@
 const portfolios = [
-	{
-		name: 'Personal Website',
-		image: 'https://i.ibb.co/dJkW5HB/daffarafi.png',
-		desc: 'My own personal website, Hope you like it :)',
-		info: {
-			date: '07/11/2022',
-			client: 'Me',
-			tools: 'HTML, CSS, Javascript, React, Tailwind, SwiperJS',
-			web: 'https://daffarafi.netlify.app',
-		},
-	},
-	{
-		name: 'Game4Later',
-		image: 'https://i.ibb.co/LC8DCdg/Game4-Later.png',
-		desc: 'Websites that made by me for learning how to fetch API',
-		info: {
-			date: '26/10/2022',
-			client: '-',
-			tools: 'HTML, CSS, Javascript, Webpack, SwiperJS, Tailwind, JQuery',
-			web: 'https://game4later.netlify.app/',
-		},
-	},
-	{
-		name: 'Bookshelf App',
-		image: 'https://i.ibb.co/5xSj24V/Bookshelf-Save-your-books.png',
-		desc: 'Websites that made by me for learning how to use Web Storage (Local Storage and Session Storage) to save data for a while. (In this case to save list of your books)',
-		info: {
-			date: '12/07/2022',
-			client: '-',
-			tools: 'HTML, CSS, Javascript',
-			web: 'https://bookshelf-urself.netlify.app/',
-		},
-	},
-	{
-		name: 'Rajawali LED',
-		image: 'https://i.ibb.co/G29nTvV/Rajawali-LED.png',
-		desc: '-',
-		info: {
-			date: '31/08/2021',
-			client: 'Rajawali LED',
-			tools: 'HTML, CSS, Javascript, JQuery, Bootstrap, GlideJS, Odometer (Library), Animate On Scroll (AOS)',
-			web: 'https://rajawali-led.github.io/',
-		},
-	},
+  {
+    name: 'Compfest 16',
+    image: 'https://i.ibb.co.com/b58PZf5/COMPFEST-16.png',
+    desc: '-',
+    info: {
+      date: '01/01/2024',
+      client: '-',
+      tools: 'HTML, CSS, Javascript, React, Tailwind, SwiperJS',
+      web: 'https://compfest.id/',
+    },
+  },
+  {
+    name: 'Betis 2024',
+    image: 'https://i.ibb.co.com/SyWxKGm/BETIS-2024.png',
+    desc: '-',
+    info: {
+      date: '01/11/2023',
+      client: '-',
+      tools: 'HTML, CSS, Javascript, React, Tailwind, SwiperJS',
+      web: 'https://betis.cs.ui.ac.id/',
+    },
+  },
+  {
+    name: 'Pacilflix',
+    image: 'https://i.ibb.co.com/Yf7gY5r/Pacilflix.png',
+    desc: '-',
+    info: {
+      date: '23/04/2024 ',
+      client: '-',
+      tools: 'HTML, CSS, Javascript, React, Tailwind, SwiperJS',
+      web: 'https://basdat-tk-e-6-pacilflix.vercel.app/',
+    },
+  },
+  {
+    name: 'Batin',
+    image: 'https://i.ibb.co.com/YD8XvRv/Batin.png',
+    desc: '-',
+    info: {
+      date: '28/03/2024',
+      client: '-',
+      tools: 'HTML, CSS, Javascript, React, Tailwind, SwiperJS',
+      web: 'https://goto.djaeger.dev/download-batin',
+    },
+  },
+  {
+    name: 'ReadMe',
+    image: 'https://i.ibb.co.com/WDSNZMs/ReadMe.png',
+    desc: '-',
+    info: {
+      date: '27/09/2023',
+      client: '-',
+      tools: 'HTML, CSS, Javascript, React, Tailwind, SwiperJS',
+      web: 'https://readme.up.railway.app/',
+    },
+  },
+  {
+    name: 'TBCheck',
+    image: 'https://i.ibb.co.com/sJ5vMgn/TBCheck.png',
+    desc: '-',
+    info: {
+      date: '26/07/2023',
+      client: '-',
+      tools: 'HTML, CSS, Javascript, React, Tailwind, SwiperJS',
+      web: 'https://play.google.com/store/apps/details?id=id.ac.ui.cs.tbcheck&pcampaignid=web_share',
+    },
+  },
+  {
+    name: 'Compfest 15',
+    image: 'https://i.ibb.co.com/qgvv84R/COMPFEST-15.png',
+    desc: '-',
+    info: {
+      date: '01/02/2023',
+      client: '-',
+      tools: 'HTML, CSS, Javascript, React, Tailwind, SwiperJS',
+      web: 'https://compfest.id/',
+    },
+  },
+  {
+    name: 'Open Karya',
+    image: 'https://i.ibb.co.com/QMFX3dg/open-karya.png',
+    desc: '-',
+    info: {
+      date: '01/02/2023',
+      client: '-',
+      tools: 'HTML, CSS, Javascript, React, Tailwind, SwiperJS',
+      web: 'https://openkarya.netlify.app/',
+    },
+  },
+  {
+    name: 'Techfest',
+    image: 'https://i.ibb.co.com/Df3369R/Tech-Fess-By-Dapa.png',
+    desc: '-',
+    info: {
+      date: '16/02/2023',
+      client: '-',
+      tools: 'HTML, CSS, Javascript, React, Tailwind, SwiperJS',
+      web: 'https://tech-fess.netlify.app/',
+    },
+  },
+  {
+    name: 'Pemira',
+    image: 'https://i.ibb.co.com/f85ZT4v/Pemira.png',
+    desc: '-',
+    info: {
+      date: '10/12/2022',
+      client: '-',
+      tools: 'HTML, CSS, Javascript, React, Tailwind, SwiperJS',
+      web: 'https://pemira.cs.ui.ac.id/',
+    },
+  },
+  {
+    name: 'Go Resto',
+    image: 'https://i.ibb.co.com/9b0sdTd/Go-Resto.png',
+    desc: '-',
+    info: {
+      date: '01/11/2022',
+      client: '-',
+      tools: 'HTML, CSS, Javascript, React, Tailwind, SwiperJS',
+      web: 'https://gorestoapp.netlify.app/',
+    },
+  },
+  {
+    name: 'Personal Website',
+    image: 'https://i.ibb.co/dJkW5HB/daffarafi.png',
+    desc: 'My own personal website, Hope you like it :)',
+    info: {
+      date: '07/11/2022',
+      client: 'Me',
+      tools: 'HTML, CSS, Javascript, React, Tailwind, SwiperJS',
+      web: 'https://daffarafi.netlify.app',
+    },
+  },
+  {
+    name: 'Game4Later',
+    image: 'https://i.ibb.co/LC8DCdg/Game4-Later.png',
+    desc: 'Websites that made by me for learning how to fetch API',
+    info: {
+      date: '26/10/2022',
+      client: '-',
+      tools: 'HTML, CSS, Javascript, Webpack, SwiperJS, Tailwind, JQuery',
+      web: 'https://game4later.netlify.app/',
+    },
+  },
+  {
+    name: 'Bookshelf App',
+    image: 'https://i.ibb.co/5xSj24V/Bookshelf-Save-your-books.png',
+    desc: 'Websites that made by me for learning how to use Web Storage (Local Storage and Session Storage) to save data for a while. (In this case to save list of your books)',
+    info: {
+      date: '12/07/2022',
+      client: '-',
+      tools: 'HTML, CSS, Javascript',
+      web: 'https://bookshelf-urself.netlify.app/',
+    },
+  },
+  {
+    name: 'Rajawali LED',
+    image: 'https://i.ibb.co/G29nTvV/Rajawali-LED.png',
+    desc: '-',
+    info: {
+      date: '31/08/2021',
+      client: 'Rajawali LED',
+      tools:
+        'HTML, CSS, Javascript, JQuery, Bootstrap, GlideJS, Odometer (Library), Animate On Scroll (AOS)',
+      web: 'https://rajawali-led.github.io/',
+    },
+  },
 ];
 
 export default portfolios;
--- src/routes/Portfolio.jsx
+++ src/routes/Portfolio.jsx
@@ -1,43 +1,43 @@
-import portfolios from "../data/data";
+import portfolios from '../data/data';
 
 const Portfolio = () => {
-    return (
-        <main className="container pt-28 font-Source_Sans_Pro overflow-x-hidden flex flex-col items-center">
-            <h2 className="text-2xl mb-4 uppercase ">Portfolio</h2>
-            <h5 className="text-4xl mb-8 uppercase font-Josefin_Sans">
-                Latest works
-            </h5>
-            <div className="items grid grid-cols-responsivePortfolio w-full justify-center gap-8 mb-20">
-                {portfolios.map((portfolio) => {
-                    return (
-                        <a
-                            href={portfolio.info.web}
-                            key={portfolio.image}
-                            className="w-full item rounded-tr-3xl rounded-bl-3xl border-2 border-[#2EE0D0] p-4 bg-[#1c3646] hover:scale-95 hover:rounded-3xl transition-all duration-300 group "
-                        >
-                            <figure className="w-full ">
-                                <div
-                                    className={`relative item-image aspect-[5/3] rounded-2xl mb-2 overflow-hidden bg-cover bg-center`}
-                                    style={{
-                                        backgroundImage: `url(${portfolio.image})`,
-                                    }}
-                                >
-                                    <div className="w-full h-full bg-black opacity-0 absolute top-0 left-0 flex justify-center items-center group-hover:opacity-50 transition-all duration-300">
-                                        <div className="text-3xl text-white translate-y-10 group-hover:translate-y-0 transition-all duration-300 delay-100">
-                                            View Detail
-                                        </div>
-                                    </div>
-                                </div>
-                                <p className="text-xl text-[#2EE0D0]">
-                                    {portfolio.name}
-                                </p>
-                            </figure>
-                        </a>
-                    );
-                })}
-            </div>
-        </main>
-    );
+  return (
+    <main className="container pt-28 font-Source_Sans_Pro overflow-x-hidden flex flex-col items-center">
+      <h2 className="text-2xl mb-4 uppercase ">Portfolio</h2>
+      <h5 className="text-4xl mb-8 uppercase font-Josefin_Sans">
+        Latest works
+      </h5>
+      <div className="items grid grid-cols-responsivePortfolio w-full justify-center gap-8 mb-20">
+        {portfolios.map((portfolio) => {
+          return (
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={portfolio.info.web}
+              key={portfolio.image}
+              className="w-full item rounded-tr-3xl rounded-bl-3xl border-2 border-[#2EE0D0] p-4 bg-[#1c3646] hover:scale-95 hover:rounded-3xl transition-all duration-300 group "
+            >
+              <figure className="w-full ">
+                <div
+                  className={`relative item-image aspect-[5/3] rounded-2xl mb-2 overflow-hidden bg-cover bg-center`}
+                  style={{
+                    backgroundImage: `url(${portfolio.image})`,
+                  }}
+                >
+                  <div className="w-full h-full bg-black opacity-0 absolute top-0 left-0 flex justify-center items-center group-hover:opacity-50 transition-all duration-300">
+                    <div className="text-3xl text-white translate-y-10 group-hover:translate-y-0 transition-all duration-300 delay-100">
+                      View Detail
+                    </div>
+                  </div>
+                </div>
+                <p className="text-xl text-[#2EE0D0]">{portfolio.name}</p>
+              </figure>
+            </a>
+          );
+        })}
+      </div>
+    </main>
+  );
 };
 
 export default Portfolio;
