a44af3827a0cf28c859b2cae9a18e28a15c9e5b0
daffarafip24 <daffarafip24@gmail.com>
2025-08-08 19:17:50 +0700

    fix: deployment

--- .next/static/chunks/app/page-1c779d7fbcb44a11.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{3755:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,6874,23))}},e=>{e.O(0,[874,441,964,358],()=>e(e.s=3755)),_N_E=e.O()}]);
